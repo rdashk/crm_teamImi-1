492ca9b153974c581081b5d177b9bb7222b54f4c
kelogub <belogub.konstantin200202@yandex.ru>
2021-09-30 20:47:53 +0800

    Создал компоненты для вывода таблиц

---
+++ resources/js/components/tools/ToolTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="d-flex flex-column container table-bordered col-12 pl-0 pr-0 text-center" id="table">
+        <div class="d-flex flex-row container col-12 pl-0 pr-0 mt-1 mb-1 fw-bold">
+            <Column value="id" class="col-3 text-center pl-0 pr-0"></Column>
+            <Column value="name" class="col-9 text-center pl-0 pr-0"></Column>
+        </div>
+        <div v-for="$data in this.rows">
+            <TableRow v-bind="$data" :resource="resource">
+            </TableRow>
+        </div>
+    </div>
+</template>
+<script>
+import Column from "../dashboard/Column";
+import TableRow from "../tools/TableRow";
+export default {
+    name: "ToolTable",
+    components: {TableRow, Column},
+    data() {
+        return {
+            rows: [],
+        };
+    },
+    props: {
+        columns: String,
+        resource: String,
+    },
+    created() {
+        axios
+            .get('http://127.0.0.1:8000/api/'+this.resource)
+            .then(response => (this.rows = response.data));
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
