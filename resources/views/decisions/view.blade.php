da2a6e4eedb808a4ff3863d01ed9af610d1f7268
kelogub <belogub.konstantin200202@yandex.ru>
2021-10-06 21:00:14 +0800

    Асинхронное добавление

--- resources/views/decisions/view.blade.php
+++ resources/views/decisions/view.blade.php
@@ -10,24 +10,22 @@ use App\Models\Decision;
  */
 ?>
 @section("content")
-    
-    {{Form::open(["action" => 'App\Http\Controllers\DecisionsController@store', "method" => "POST", "class" => "container"])}}
-    @csrf
 
     <div class="container col-5">
         <div class="form-group">
             <div class="row mb-3">
                 <div class="col">
-                    {{Form::text("new_decision", null, ["placeholder" => "Новое решение", "class" => "form-control", "id"=>"new_decision"])}}
+                    <input type="text" placeholder="Новое решение" class="form-control" id="new_decision">
                 </div>
-                <div class="col">
-                    <button type="submit" class="btn btn-outline-dark">Добавить</button>
+                <div class="col" id="submit">
+                    <button type="submit" class="btn btn-outline-dark" v-if="!load" v-on:click="click">Добавить</button>
+                    <div class="spinner-border btn btn-outline-dark" role="status" v-if="load">
+                        <span class="sr-only"></span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    {{Form::close()}}
 
     <div id="toolTable" class="container col-5">
         <v-tool-table columns="id name" resource="decisions"></v-tool-table>
@@ -38,6 +36,35 @@ use App\Models\Decision;
     <script>
         const table = new Vue({
             el: "#toolTable",
+        })
+
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = document.head.querySelector('meta[name="csrf-token"]');
+
+        const button = new Vue({
+            el: "#submit",
+            data: {
+                load: false
+            },
+            methods: {
+                click(){
+                    this.load = true;
+                    let elem = document.getElementById('new_decision');
+                    elem.disabled = true;
+                    axios
+                        .post('http://127.0.0.1:8000/api/decisions', {name: elem.value})
+                        .then(response => {
+                            console.log(response.data);
+                            this.load = false;
+                            elem.value = "";
+                            elem.disabled = false;
+                            table.$children[0].load();
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            this.load = false;
+                        })
+                }
+            }
         })
     </script>
 @endpush
