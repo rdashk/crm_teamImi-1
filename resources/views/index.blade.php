0b0c49e2d701705c409555755f238df499e76979
rdashk <rdashk8@gmail.com>
2021-10-06 10:08:13 +0800

    Установлена начальная страница - Dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,5 @@
 @endsection
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row justify-content-around">
-            <a class="btn col-2 btn-outline-dark" href="/briefs">DashBoard</a>
-            <a class="btn col-2 btn-outline-dark" href="/decisions">Решения</a>
-            <a class="btn col-2 btn-outline-dark" href="/levels">Уровни</a>
-            <a class="btn col-2 btn-outline-dark" href="/positions">Позиции</a>
-        </div>
-    </div>
+    @include('briefs/view')
 @endsection
