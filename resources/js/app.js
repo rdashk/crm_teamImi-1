9a0662af0b793011235775da63103de3ef3c8029
kelogub <belogub.konstantin200202@yandex.ru>
2021-10-16 19:06:42 +0800

    долгие нажатия (?)

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import Vue2TouchEvents from 'vue2-touch-events';
+Vue.use(Vue2TouchEvents);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/dashboard/TableRow.vue
+++ resources/js/components/dashboard/TableRow.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="compClass" @click="click" v-on:mouseover="mouseOver"
-         v-on:mouseleave="mouseLeave"
+    <div :class="compClass" @click="click" v-touch:touchhold="mouseOver" v-on:mouseover="mouseOver"
+         v-on:mouseleave="mouseLeave" v-touch:end="mouseLeave"
          style="cursor: pointer;">
         <transition name="slide-left-fade">
             <div :class="buttonsClass" v-if="show" style="position: absolute; left: -2.1em;">
